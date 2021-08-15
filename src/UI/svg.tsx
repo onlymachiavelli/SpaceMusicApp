dc4461b22684401cb19e4d739d4c55048a2e56d1
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-15 23:25:24 +0100

    Icons SVG

--- src/UI/svg.tsx
+++ src/UI/svg.tsx
@@ -306,3 +306,26 @@ export const Liluser = () => {
 
     )
 }
+
+export const Hidden = () =>{
+    return(
+        
+<Svg width="22" height="9" viewBox="0 0 22 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Path d="M1 1C1 1 4.5 5 11 5C17.5 5 21 1 21 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M3 2.645L1 5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M21 5L19.004 2.648" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M7.914 4.68L7 7.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M14.063 4.688L15 7.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</Svg>
+
+    )
+}
+export const Visible =() =>{
+    return(
+        
+<Svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Path d="M21.257 10.962C21.731 11.582 21.731 12.419 21.257 13.038C19.764 14.987 16.182 19 12 19C7.81801 19 4.23601 14.987 2.74301 13.038C2.51239 12.7411 2.38721 12.3759 2.38721 12C2.38721 11.6241 2.51239 11.2589 2.74301 10.962C4.23601 9.013 7.81801 5 12 5C16.182 5 19.764 9.013 21.257 10.962V10.962Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</Svg>
+    )
+}
