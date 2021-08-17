1a48ae14f19e543383bc789a4d27ef30955c9ee2
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-17 23:09:56 +0100

    Home scroll

--- src/UI/home.tsx
+++ src/UI/home.tsx
@@ -1,9 +1,13 @@
 import React, {useState, useRef, useEffect} from "react"
 import {} from "./svg"
 import Language from "../../packs/languages"
-
+import {View, Text, ScrollView, TouchableOpacity, } from 'react-native'
 
 const Home = () =>{
-    return()
+    return(
+        <ScrollView>
+            
+        </ScrollView>
+    )
 }
 export default Home
