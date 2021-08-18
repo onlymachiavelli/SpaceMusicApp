b20f2bd04bd9f7103ea8b18367d4805835cf878f
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-18 23:35:01 +0100

    dimensions

--- src/UI/ifConnected/main.tsx
+++ src/UI/ifConnected/main.tsx
@@ -1,6 +1,7 @@
 
 import { Dimensions, StyleSheet } from "react-native"
-
+const Width = Dimensions.get("window").width
+const Height = Dimensions.get("window").height
 
 export const HomeStyle = StyleSheet.create({
     
